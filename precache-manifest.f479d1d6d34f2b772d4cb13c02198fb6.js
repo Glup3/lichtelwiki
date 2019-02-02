645c010130b0121968330fd2103750870aebcc65
Phuc Tran <phuc@Phucs-MacBook-Pro.local>
2019-02-02 21:47:24 +0100

    Updates

---
+++ precache-manifest.f479d1d6d34f2b772d4cb13c02198fb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ddd46bcaf5e507bdd76",
+    "url": "/lichtelwiki/static/js/runtime~main.6ddd46bc.js"
+  },
+  {
+    "revision": "bfa9c7399e1c00d40971",
+    "url": "/lichtelwiki/static/js/main.bfa9c739.chunk.js"
+  },
+  {
+    "revision": "9dfbfc4b77ccde6d8c87",
+    "url": "/lichtelwiki/static/js/1.9dfbfc4b.chunk.js"
+  },
+  {
+    "revision": "bfa9c7399e1c00d40971",
+    "url": "/lichtelwiki/static/css/main.6559372a.chunk.css"
+  },
+  {
+    "revision": "9dfbfc4b77ccde6d8c87",
+    "url": "/lichtelwiki/static/css/1.2cdf9467.chunk.css"
+  },
+  {
+    "revision": "cd569bcd41ee061f1e78368b93a95f6c",
+    "url": "/lichtelwiki/index.html"
+  }
+];
